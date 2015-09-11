d3371a0e07141185f3dd1e146de91b7e167a2504
Aashish <aashishkatlam@gmail.com>
2015-09-11 19:37:34 +0530

    Responsive Fix

--- public/js/about.js
+++ public/js/about.js
@@ -2,16 +2,29 @@
  * Created by aashish on 6/9/15.
  */
 
-var app = angular.module('myApp',['ngMaterial']);
+var app = angular.module('myApp',['ngMaterial','duScroll','ngMdIcons']) .value('duScrollDuration', 2000)
+    .value('duScrollOffset', 30);
 
-app.controller('aController',function($scope,$http){
+app.controller('aController',function($scope,$http,$document,$mdSidenav){
     console.log('App running');
 
 
     var isConnected = false;
     var pid = null;
+    $scope.toggleSidenav = toggleSidenav;
+
+    function toggleSidenav(name) {
+        $mdSidenav(name).toggle();
+    }
 
    $scope.title="About";
+
+    $scope.gotoTop = function() {
+
+        $document.scrollTopAnimated(0, 500).then(function() {
+            console && console.log('You just scrolled to the top!');
+        });
+    };
 
 
     /**
@@ -81,9 +94,28 @@ app.controller('aController',function($scope,$http){
 
 });
 
+app.directive("scroll", function ($window) {
+    return function(scope, element, attrs) {
+
+        angular.element($window).bind("scroll", function() {
+            if(this.pageYOffset >= 250){
+                scope.boolChangeClass = true;
+                console.log("ToolBar");
+            }
+            else if (this.pageYOffset >= 500) {
+
+                console.log("FAB Visible");
+            } else {
+                scope.boolChangeClass = false;
+            }
+            scope.$apply();
+        });
+    };
+});
+
 app.config(function($mdThemingProvider) {
 
-    var myColor = $mdThemingProvider.extendPalette('blue', {
+    var myColor = $mdThemingProvider.extendPalette('blue-grey', {
         '500': '455A64'
     });
 
